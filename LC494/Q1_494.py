0ff16496de76d03392b49a9beb3063368f8de60a
RajVarun <grajvarun@gmail.com>
2026-03-22 08:04:59 +0530

    Q1_494.py

---
+++ LC494/Q1_494.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # nums1 contains distinct integers of n.
+        # construnct anotehr array of nums 2 of len n, 
+        # nums2 are either all orr do even.
+        return True
+    
